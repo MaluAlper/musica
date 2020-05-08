4df5592643c23d0efffb904ce2432d84f0e4453d
Maria Luiza <malualper@hotmail.com>
2020-05-07 21:55:38 -0300

    Musicas

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Nossa, nossa");
+            Console.WriteLine("Assim você me mata");
+            Console.WriteLine("Ai, se eu te pego");
+            Console.WriteLine("Ai, ai se eu te pego");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Delícia, delícia");
+            Console.WriteLine("Assim você me mata");
+            Console.WriteLine("Ai, se eu te pego");
+            Console.WriteLine("Ai, ai, se eu te pego");
+            Console.ResetColor();
+
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Sábado na balada");
+            Console.WriteLine("A galera começou a dançar");
+            Console.WriteLine("E passou a menina mais linda");
+            Console.WriteLine("Tomei coragem e comecei a falar");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Nossa, nossa");
+            Console.WriteLine("Assim você me mata");
+            Console.WriteLine("Ai, se eu te pego");
+            Console.WriteLine("Ai, ai se eu te pego");
+            Console.ResetColor();
+
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Delícia, delícia");
+            Console.WriteLine("Assim você me mata");
+            Console.WriteLine("Ai, se eu te pego");
+            Console.WriteLine("Ai, ai, se eu te pego");
+            Console.ResetColor();
+
+            
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Sábado na balada");
+            Console.WriteLine("A galera começou a dançar");
+            Console.WriteLine("E passou a menina mais linda");
+            Console.WriteLine("Tomei coragem e comecei a falar");
+            Console.ResetColor();
+
+
+           
+    }
+  }
+}
